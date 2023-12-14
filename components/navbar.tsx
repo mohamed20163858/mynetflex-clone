bd8b3016ae15b68ea636d9ed00103c01d5254abf
Mohamed Saleh <mohamed20163858@gmail.com>
2023-12-14 15:46:38 +0200

    Create Navbar part 1

---
+++ components/navbar.tsx
@@ -0,0 +1,68 @@
+import NavbarItem from "./navbarItem";
+import { BsSearch, BsBell, BsChevronDown } from "react-icons/bs";
+import MobileMenu from "./MobileMenu";
+import { useCallback, useState } from "react";
+import AccountMenu from "./AccountMenu";
+const Navbar = () => {
+    const [ showMobileMenu, setShowMobileMenu]  = useState(false);
+    const toggleMobileMenu =  useCallback(() => {
+        setShowMobileMenu((current) => !current);
+    }, []);
+    return (
+    <nav className="w-full z-40 fixed">
+        <div className="
+            px-4
+            md:px-16
+            py-6
+            flex
+            flex-row
+            items-center
+            transition
+            duration-500
+            bg-zinc-900
+            bg-opacity-90
+
+        ">
+            <img src="/images/logo.png" alt="logo" className="
+                h-4
+                lg:h-7
+            " />
+            <div className="
+                flex-row
+                ml-8
+                gap-7
+                hidden
+                lg:flex 
+            ">
+                <NavbarItem label="Home" />
+                <NavbarItem label="Series" />
+                <NavbarItem label="Films" />
+                <NavbarItem label="New & Popular" />
+                <NavbarItem label="My List" />
+                <NavbarItem label="Browse by languages" />
+            </div>
+            <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+                <p className="text-white text-sm">Browse</p>
+                <BsChevronDown className="text-white transition" />
+                <MobileMenu visible={showMobileMenu} />
+            </div>
+            <div className="flex flex-row ml-auto gap-7 items-center">
+                <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+                    <BsSearch />
+                </div>
+                <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+                    <BsBell />
+                </div>
+                <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+                    <div className="h-6 w-6 lg:h-10 lg:w-10 overflow-hidden rounded-md">
+                        <img src="/images/default-blue.png" alt="person" />
+                    </div>
+                    <BsChevronDown className="text-white transition" />
+                    <AccountMenu />
+                </div>
+            </div>
+        </div>
+    </nav>
+    );
+}
+export default Navbar;
